547ca9d1de742132d259a2ecae0ed1f88388a129
Sinthon Seng <s.sinthon@cas.com.kh>
2024-09-25 16:53:31 +0700

    New feature => Add SimpleInjectorDI

---
+++ SimpleInjectorDI/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Common;
+using DevExpress.Mvvm.POCO;
+using SimpleInjector;
+using System;
+using System.Windows;
+
+namespace SimpleInjectorDI;
+
+public partial class App : Application
+{
+    private Container _container = new Container();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _container.RegisterSingleton(
+            typeof(IDataStorage<Person>),
+            typeof(PersonStorage));
+
+        _container.RegisterSingleton(
+            typeof(DetailViewModel),
+            ViewModelSource.GetPOCOType(typeof(DetailViewModel)));
+
+        _container.RegisterSingleton(
+            typeof(IDetailViewModel),
+            ViewModelSource.GetPOCOType(typeof(DetailViewModel)));
+
+        _container.Register(
+            typeof(CollectionViewModel),
+            ViewModelSource.GetPOCOType(typeof(CollectionViewModel)));
+
+        DISource.Resolver = Resolve;
+    }
+
+    private object Resolve(Type type, object key, string name)
+    {
+        if (type == null)
+            return null;
+
+        return _container.GetInstance(type);
+    }
+}
